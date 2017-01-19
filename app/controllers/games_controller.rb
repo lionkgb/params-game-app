947bf8d78d349638c8a2327e273913cec2e30a28
lionkgb <lionceau.lk@gmail.com>
2017-01-19 16:08:47 +0200

    Added a parameter

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,8 @@
 class GamesController < ApplicationController
+  def query_params
+   @name = params[:name]
+   puts "below are your names capitalized"
+   puts @name
+   render "query_params.html.erb"
+  end
 end
