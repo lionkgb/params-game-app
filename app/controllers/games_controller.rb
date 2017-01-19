3c705ca1c75f8e8e1c322e1183e5d5f912aff83b
lionkgb <lionceau.lk@gmail.com>
2017-01-19 16:14:38 +0200

    Added if method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def query_params
    @name = params[:name]
-   puts "below are your names capitalized"
+   puts "below are your names"
    puts @name
    render "query_params.html.erb"
   end
